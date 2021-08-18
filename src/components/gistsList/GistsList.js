11f19c9a275f6fc9e32ee16040aa514dc8344307
Armin <arminb997@gmail.com>
2021-08-18 21:10:26 +0200

    Implement service to fetch data using the GitHub API
    The service is implemented and currently logs the fetched data
    in the console.
    
    Resolves: #2

--- src/components/gistsList/GistsList.js
+++ src/components/gistsList/GistsList.js
@@ -1,6 +1,26 @@
 import "./gists-list.scss";
+import { useState, useEffect } from "react";
+import api from "../../api/gists";
 
 const GistsList = () => {
+  const [gists, setGists] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const listGists = async () => {
+    const fetchedData = await api.get("1970-01-01", 30, currentPage);
+
+    setGists(fetchedData.data);
+    setIsLoading(false);
+    console.log(fetchedData.data);
+  };
+
+  useEffect(() => {
+    listGists().catch((error) => {
+      console.log(error);
+    });
+  }, [currentPage]);
+
   return (
     <div>
       <p>gists list</p>
